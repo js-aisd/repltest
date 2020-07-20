10f889a7f30f39b71796239abe2f67fbed7c5588
James Shockey <james.shockey@austinisd.org>
2020-07-20 20:42:46 +0000

    Changed to FX hello

--- Main.java
+++ Main.java
@@ -1,8 +1,40 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+
+/**
+ * 
+ * This class demonstrates the simple hello world swing program.
+ * @Ramesh Fadatare
+ */
 public class Main {
 
-  public static void main (String [] args)
-  {
-    System.out.println("Hello java!");
-  }
+    private static void createAndShowGUI() {
+        JFrame jFrame = new JFrame("Hello World Swing Example");
+        jFrame.setLayout(new FlowLayout());
+        jFrame.setSize(600, 360);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+        JLabel label = new JLabel("Hello World Swing?");
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+        label.setBorder(border);
+        label.setPreferredSize(new Dimension(150, 100));
+
+        label.setText("Hello World Swing!");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+
+        jFrame.add(label);
+        jFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        createAndShowGUI();
+    }
 }
